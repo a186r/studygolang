bb35095ff82a484fe626d7e923440b25b517936f
a186r <hi.aewalle@gmail.com>
2019-05-05 15:12:05 +0800

    commit invert map

--- day13/invert_map.go
+++ day13/invert_map.go
@@ -1,3 +1,4 @@
+// 如果map的值类型可以作为key并且所有的value是唯一的，那么通过下面的方法可以简单的做到键值对调
 package main
 
 import "fmt"
@@ -20,3 +21,7 @@ func main() {
 		fmt.Printf("Key: %v, Value:%v / ", k, v)
 	}
 }
+
+// 如果原始的value值不唯一，那么这么做肯定会出问题，这种情况下不会报错，但是当遇到不唯一的key时应当直接
+// 停止对调，且此时对调后的map很可能没有包含原map的所有键值对，一种解决方法就是仔细检查唯一性并且使用多值
+// map，比如使用map[int][]string类型
