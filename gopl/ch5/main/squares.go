385321e21853778482f9bb97ab2bb04697252096
a186r <hi.aewalle@gmail.com>
2019-09-20 13:48:05 +0800

    squares

---
+++ gopl/ch5/main/squares.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//squares返回一个匿名函数，该匿名函数被调用的时候，都会返回下一个数的平方
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
+
+func main(){
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+}
