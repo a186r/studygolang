d69918f27f6336a9e9b63b9a98d9b3965504bd69
a186r <hi.aewalle@gmail.com>
2019-09-29 09:52:27 +0800

    defer用法

--- gopl/ch5/title1.go
+++ gopl/ch5/title1.go
@@ -3,8 +3,11 @@ package ch5
 import (
 	"fmt"
 	"html"
+	"log"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 )
 
 func title1(url string) error {
@@ -27,3 +30,57 @@ func title1(url string) error {
 		return fmt.Errorf("parsing %s as HTML: %v", url, err)
 	}
 }
+
+func bigSlowOperation() {
+	defer trace("bigSlowOperation")()
+	time.Sleep(10 * time.Second)
+}
+
+//trace记录函数退出的时间，并输出总耗时
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() {
+		log.Printf("exit %s (%s)", msg, time.Since(start))
+	}
+}
+
+//func double(x int) int {
+//	return x + x
+//}
+
+func double(x int) (result int) {
+	defer func() { fmt.Printf("double(%d) = %d/n", x, result) }()
+	return x + x
+}
+
+func triple(x int) (result int) {
+	defer func() {result += x}()
+	return double(x)
+}
+
+//fmt.Println(triple(4)) // 12
+
+//在循环体中使用defer语句要特别注意，因为defer会在整个循环结束后才会调用，可能会导致很多资源没有关闭，可以使用下面
+//的处理方法，在循环体外再写一个方法，在该方法中调用defer
+
+func deferTest(filenames []string) error {
+	for _, filename := range filenames{
+		if err := doFile(filename); err != nil{
+			return err
+		}
+	}
+	return nil
+}
+
+//
+func doFile(filename string) error{
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+}
+
+
--- gopl/ch5/wait.go
+++ gopl/ch5/wait.go
@@ -8,9 +8,9 @@ import (
 	"time"
 )
 
-func main(){
+func main() {
 	url := "www.google.com"
-	if err := WaitForServer(url); err != nil{
+	if err := WaitForServer(url); err != nil {
 		fmt.Fprintf(os.Stderr, "Site is down: %v\n", err)
 		os.Exit(1)
 	}
@@ -20,9 +20,9 @@ func main(){
 func WaitForServer(url string) error {
 	const timeout = 1 * time.Minute
 	deadline := time.Now().Add(timeout)
-	for tries := 0; time.Now().Before(deadline); tries++{
+	for tries := 0; time.Now().Before(deadline); tries++ {
 		_, err := http.Head(url)
-		if err == nil{
+		if err == nil {
 			return nil
 		}
 		log.Printf("server not responding (%s);retrying...", err)
